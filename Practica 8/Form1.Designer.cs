37d48ddd9b91bcf4d13ea119d4e6aee9bb61bf2a
emmanuelvj17 <148415063+emmanuelvj17@users.noreply.github.com>
2023-11-17 19:30:26 -0600

    Add files via upload

---
+++ Practica 8/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Practica_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(210, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(215, 16);
+            label1.TabIndex = 0;
+            label1.Text = "Este programa escribe del 1 al 5.";
+            // 
+            // listBox1
+            // 
+            listBox1.Font = new Font("Arial", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 16;
+            listBox1.Location = new Point(223, 118);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(148, 228);
+            listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Red;
+            button1.Font = new Font("Arial", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(425, 181);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 47);
+            button1.TabIndex = 2;
+            button1.Text = "Listar numeros";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 255, 192);
+            ClientSize = new Size(649, 409);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListBox listBox1;
+        private Button button1;
+    }
+}
